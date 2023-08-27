4d57ff11717abe5814a5f79003c67f02183dc635
taufik-hdyt <taufikhdyt2599@gmail.com>
2023-08-27 14:10:54 +0700

    task 12 node js crud

---
+++ fake-data.js
@@ -0,0 +1,47 @@
+const dataProject = [
+  {
+    // id: 1,
+    name: "Create Card",
+    description: "pertemuan 1 latihan membuat card",
+    duration: "1 hari",
+    start_date: "2023-08-26",
+    end_date: "2023-08-27",
+    tech: {
+      nodeJs: true,
+      reactJs: true,
+      php: false,
+      javascript: true,
+    },
+  },
+  {
+    // id: 2,
+    name: "Create Array Of Object",
+    description:
+      "pertemuan 2 latihan membuat array of object dan method dan mengimplementasikan",
+    duration: "1 Tahun",
+    start_date: "2023-08-26",
+    end_date: "2024-08-26",
+    tech: {
+      nodeJs: true,
+      reactJs: true,
+      php: false,
+      javascript: true,
+    },
+  },
+  {
+    // id: 3,
+    name: "Node js With Express JS",
+    description: "Latihan membuat web server menggunakan express js",
+    duration: "1 Bulan",
+    start_date: "2023-08-26",
+    end_date: "2023-09-26",
+    tech: {
+      nodeJs: true,
+      reactJs: true,
+      php: false,
+      javascript: true,
+    },
+  },
+];
+
+module.exports = dataProject;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 const path = require("path");
+const dataProject = require("./fake-data");
 
 // html to hbs
 app.set("view engine", "hbs");
@@ -17,52 +18,112 @@ app.get("/", home);
 app.get("/project", project);
 app.post("/project", addProject);
 app.get("/contact", contact);
+app.post("/contact", contactPost);
 app.get("/project-detail/:id", projectDetail);
+app.get("/delete-project/:id", deleteProject);
+app.get("/update-project/:id", updateProject);
+app.post("/update-project/:id", updateNewProject);
 
 // server port
 app.listen(PORT, () => console.log(`Server running on port${PORT}`));
 
 // callback function router
-function home(req, res) {
-  res.render("index");
+
+function contactPost(req, res) {
+  const { name, email, phone, subject, message } = req.body;
+  if (
+    (name !== "", email !== "", phone !== "", subject !== "", message !== "")
+  ) {
+    res.redirect(
+      `mailto: taufikhdyt@gmail.com?subject=${subject}&body= Halo Nama saya ${name}, ${message} Silahkan Hubungi ${phone}`
+    );
+  } else {
+    console.log("Data is empty");
+  }
 }
+
+function home(req, res) {
+  res.render("index", { dataProject });
+}
+
 function project(req, res) {
-  res.render("project");
+  const methodPost = "post";
+  res.render("project", { method: methodPost });
+}
+
+function duration(startDate, endDate) {
+  let start = new Date(startDate);
+  let end = new Date(endDate);
+  let miliDetik = end - start;
+  const hari = miliDetik / (1000 * 60 * 60 * 24);
+
+  if (hari < 30) {
+    return `${hari} Hari`;
+  } else if (hari <= 360) {
+    return `${Math.floor(hari / 30)} Bulan`;
+  } else if (hari >= 360) {
+    return `${Math.floor(hari / 30 / 12)} Tahun`;
+  }
 }
 function addProject(req, res) {
-  const {
+  const { name, startDate, endDate, description } = req.body;
+  const durasi = duration(startDate, endDate);
+  const data = {
     name,
-    startDate,
-    endDate,
     description,
-    // nodeJs,
-    // php,
-    // reactJs,
-    // javascript,
-  } = req.body;
-  console.log(
-    name,
-    startDate,
-    endDate,
-    description
-    // nodeJs,
-    // php,
-    // reactJs,
-    // javascript
-  );
+    duration: durasi,
+    start_date: startDate,
+    end_date: endDate,
+    tech: {
+      nodeJs: req.body.nodeJs !== undefined,
+      reactJs: req.body.reactJs !== undefined,
+      php: req.body.php !== undefined,
+      javascript: req.body.javascript !== undefined,
+    },
+  };
+
+  dataProject.push(data);
   res.redirect("/");
 }
 function contact(req, res) {
   res.render("contact");
 }
+
 function projectDetail(req, res) {
-  const id = req.params;
-  const data = {
-    id,
-    title: "Dumbways Project",
-    content:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquaaccusamus sintDolor modi repudiandae",
-    image: "https://i.imgur.com/1dlelFG.jpg",
+  const { id } = req.params;
+  res.render("project-detail", { data: dataProject[id] });
+}
+
+function deleteProject(req, res) {
+  const { id } = req.params;
+  dataProject.splice(id, 1);
+  res.redirect("/");
+}
+
+function updateProject(req, res) {
+  const { id } = req.params;
+  const methodPut = "put";
+  res.render("project", { methodPut, data: dataProject[id] });
+}
+function updateNewProject(req, res) {
+  const { id } = req.params;
+  const { name, description, startDate, endDate } = req.body;
+  const durasi = duration(startDate, endDate);
+  const newData = {
+    name,
+    description,
+    duration: durasi,
+    start_date: startDate,
+    end_date: endDate,
+    tech: {
+      nodeJs: req.body.nodeJs !== undefined,
+      reactJs: req.body.reactJs !== undefined,
+      php: req.body.php !== undefined,
+      javascript: req.body.javascript !== undefined,
+    },
   };
-  res.render("project-detail", { data });
+
+  dataProject.push(newData);
+  dataProject.splice(id, 1);
+  res.redirect("/");
 }
